d2cbcbbd268b6ddcb81c8ed4c371f6cf963e8bd5
ronak083 <ronakgupta083@gmail.com>
2023-09-20 20:46:37 +0530

    Delete Call

--- src/main/java/com/invoiceprocessing/server/controller/InvoiceController.java
+++ src/main/java/com/invoiceprocessing/server/controller/InvoiceController.java
@@ -2,11 +2,12 @@ package com.invoiceprocessing.server.controller;
 import com.invoiceprocessing.server.model.Invoice;
 import com.invoiceprocessing.server.services.InvoiceService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@CrossOrigin
 public class InvoiceController {
 
     @Autowired
@@ -15,5 +16,18 @@ public class InvoiceController {
     public Invoice addInvoice(@RequestBody Invoice invoice) {
         return this.invoiceService.addInvoice(invoice);
     }
+
+    @GetMapping("/invoice")
+    public List<Invoice> getInvoices(){
+        return this.invoiceService.getInvoices();
+
+    }
+
+    @DeleteMapping("/invoice/{invoiceId}")
+    public Invoice deleteInvoice(@PathVariable String invoiceId){
+        return this.invoiceService.deleteInvoice(Long.parseLong(invoiceId));
+    }
+
+
 
 }
